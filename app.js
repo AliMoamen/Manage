f39b98c57474bc469d193840526f73afb4fa28df
Ali Moamen <alimoamen100@gmail.com>
2021-10-14 23:09:00 +0200

    Changing Port

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static("public"))
 app.get("/" , function(req , res){
   res.sendFile(__dirname + "/index.html")
 })
-app.listen(3000 || process.env.PORT , function(req , res){
+app.listen(process.env.PORT ||3000  , function(req , res){
   console.log("Server is running on port 3000")
 })
